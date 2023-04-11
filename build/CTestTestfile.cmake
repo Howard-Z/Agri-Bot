38013139ca0e3dd418b7580e407dffd2931917f4
howard <howard@Ubuntu.myguest.virtualbox.org>
2023-04-11 03:19:26 -0700

    Created Catkin Workspace

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/howard/Documents/Agri-Bot/src
+# Build directory: /home/howard/Documents/Agri-Bot/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
