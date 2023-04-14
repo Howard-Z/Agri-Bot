93efa1a8052a30be3e6065bb0c45140810ba8500
Howard-Z <67174263+Howard-Z@users.noreply.github.com>
2023-04-14 12:12:11 -0700

    Initial Branch Push
    Starting code for CRSF and config

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Agri-Bot
+
+## ESP32 code
+
+(include wiring diagrams at some point)
--- build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/howard/Documents/Agri-Bot/src
-# Build directory: /home/howard/Documents/Agri-Bot/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
