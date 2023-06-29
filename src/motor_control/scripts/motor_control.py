13ca87165f7558ccb42a4c35f93a9b685b630036
Howard Zhu <howardzhu8@gmail.com>
2023-06-29 00:59:55 -0700

    Added Sprayer Code

--- src/motor_control/scripts/motor_control.py
+++ src/motor_control/scripts/motor_control.py
@@ -7,84 +7,105 @@ from std_msgs.msg import String, Int8
 
 ch = []
 last_call_time = 0
-wait_time = .01 #in seconds
+wait_time = .01  # in seconds
+
 
 def channel(i):
     return ch[i - 1]
 
+
 def callback(input_data):
-    global ch
-    global last_call_time
-    current_time = time.time()
-    if current_time - last_call_time > wait_time:
-      #last_call_time = current_time
-      ch = input_data.data.split(',') #stupid ROS msg stuff
-      if channel(5) == "1000":
-          manual_control()
-      else:
-          return
-    return
+	global ch
+	global last_call_time
+	current_time = time.time()
+	if current_time - last_call_time > wait_time:
+		# last_call_time = current_time
+		ch = input_data.data.split(',')  # stupid ROS msg stuff
+		# sprayer logic
+		temp = 0
+		if int(channel(7)) >= 1050:
+			temp += 1
+		if int(channel(8)) >= 1050:
+			temp += 2
+		if int(channel(9)) >= 1050:
+			temp += 4
+		if int(channel(10)) >= 1050:
+			temp += 8
+		sendSprayer(temp)
+		if int(channel(5)) <= 1050:  # 1050 is an arbitrary number for a threshold
+			manual_control()
+		else:
+			return
+		return
 
-#Bless https://www.instructables.com/Joystick-to-Differential-Drive-Python/
-def joystickToDiff(x, y, minJoystick, maxJoystick, minSpeed, maxSpeed): # If x and y are 0, then there is not much to calculate...
-	if x == 0 and y == 0:
-		return (0, 0)
-    
 
-	# First Compute the angle in deg
-	# First hypotenuse
-	z = math.sqrt(x * x + y * y)
+def sendSprayer(data):
+	Sprayer = rospy.Publisher('/Sprayer', Int8, queue_size=1)
+	Sprayer.publish(data)
+	return
 
-	# angle in radians
-	rad = math.acos(math.fabs(x) / z)
 
-	# and in degrees
-	angle = rad * 180 / math.pi
+# Bless https://www.instructables.com/Joystick-to-Differential-Drive-Python/
+def joystickToDiff(x, y, minJoystick, maxJoystick, minSpeed,
+                   maxSpeed):  # If x and y are 0, then there is not much to calculate...
+    if x == 0 and y == 0:
+        return (0, 0)
 
-	# Now angle indicates the measure of turn
-	# Along a straight line, with an angle o, the turn co-efficient is same
-	# this applies for angles between 0-90, with angle 0 the coeff is -1
-	# with angle 45, the co-efficient is 0 and with angle 90, it is 1
+    # First Compute the angle in deg
+    # First hypotenuse
+    z = math.sqrt(x * x + y * y)
 
-	tcoeff = -1 + (angle / 90) * 2
-	turn = tcoeff * math.fabs(math.fabs(y) - math.fabs(x))
-	turn = round(turn * 100, 0) / 100
+    # angle in radians
+    rad = math.acos(math.fabs(x) / z)
 
-	# And max of y or x is the movement
-	mov = max(math.fabs(y), math.fabs(x))
+    # and in degrees
+    angle = rad * 180 / math.pi
 
-	# First and third quadrant
-	if (x >= 0 and y >= 0) or (x < 0 and y < 0):
-		rawLeft = mov
-		rawRight = turn
-	else:
-		rawRight = mov
-		rawLeft = turn
+    # Now angle indicates the measure of turn
+    # Along a straight line, with an angle o, the turn co-efficient is same
+    # this applies for angles between 0-90, with angle 0 the coeff is -1
+    # with angle 45, the co-efficient is 0 and with angle 90, it is 1
 
-	# Reverse polarity
-	if y < 0:
-		rawLeft = 0 - rawLeft
-		rawRight = 0 - rawRight
+    tcoeff = -1 + (angle / 90) * 2
+    turn = tcoeff * math.fabs(math.fabs(y) - math.fabs(x))
+    turn = round(turn * 100, 0) / 100
 
-	# minJoystick, maxJoystick, minSpeed, maxSpeed
-	# Map the values onto the defined rang
-	rightOut = map(rawRight, minJoystick, maxJoystick, minSpeed, maxSpeed)
-	leftOut = map(rawLeft, minJoystick, maxJoystick, minSpeed, maxSpeed)
+    # And max of y or x is the movement
+    mov = max(math.fabs(y), math.fabs(x))
 
-	return (rightOut, leftOut)
+    # First and third quadrant
+    if (x >= 0 and y >= 0) or (x < 0 and y < 0):
+        rawLeft = mov
+        rawRight = turn
+    else:
+        rawRight = mov
+        rawLeft = turn
+
+    # Reverse polarity
+    if y < 0:
+        rawLeft = 0 - rawLeft
+        rawRight = 0 - rawRight
+
+    # minJoystick, maxJoystick, minSpeed, maxSpeed
+    # Map the values onto the defined rang
+    rightOut = map(rawRight, minJoystick, maxJoystick, minSpeed, maxSpeed)
+    leftOut = map(rawLeft, minJoystick, maxJoystick, minSpeed, maxSpeed)
+
+    return (rightOut, leftOut)
+
 
 def map(v, in_min, in_max, out_min, out_max):
-	# Check that the value is at least in_min
-	if v < in_min:
-		v = in_min
-	# Check that the value is at most in_max
-	if v > in_max:
-		v = in_max
-	return (v - in_min) * (out_max - out_min) // (in_max - in_min) + out_min
+    # Check that the value is at least in_min
+    if v < in_min:
+        v = in_min
+    # Check that the value is at most in_max
+    if v > in_max:
+        v = in_max
+    return (v - in_min) * (out_max - out_min) // (in_max - in_min) + out_min
 
 
-#totally can do better since the motor speeds depends on the width of the robot...
-#this is a temp fix for now
+# totally can do better since the motor speeds depends on the width of the robot...
+# this is a temp fix for now
 def manual_control():
     global ch
     FL = rospy.Publisher('/Motors/FL', Int8, queue_size=1)
@@ -94,27 +115,28 @@ def manual_control():
     ch1 = int(channel(1)) - 1500
     ch2 = int(channel(2)) - 1500
     throttle = joystickToDiff(ch1, ch2, -511, 511, -127, 127)
-    #print(throttle)
+    # print(throttle)
     FL.publish(int(throttle[0]))
     BL.publish(int(throttle[0]))
     FR.publish(int(throttle[1]))
     BR.publish(int(throttle[1]))
-    #FIX DEBOUNCING ISSUE USING MILLIS()
+    # FIX DEBOUNCING ISSUE USING MILLIS()
     # unsigned long lastExecuted = 0;
     # unsigned long debounceDelay = 1000; // debounce delay in milliseconds
 
-    
+
 def main():
+    # In ROS, nodes are uniquely named. If two nodes with the same
+    # name are launched, the previous one is kicked off. The
+    # anonymous=True flag means that rospy will choose a unique
+    # name for our 'listener' node so that multiple listeners can
+    # run simultaneously.
+    rospy.init_node('motor_control')
 
-  # In ROS, nodes are uniquely named. If two nodes with the same
-  # name are launched, the previous one is kicked off. The
-  # anonymous=True flag means that rospy will choose a unique
-  # name for our 'listener' node so that multiple listeners can
-  # run simultaneously.
-  rospy.init_node('motor_control')
+    rospy.Subscriber("/RC/CH", String, callback)
+    # spin() simply keeps python from exiting until this node is stopped
+    rospy.spin()
 
-  rospy.Subscriber("/RC/CH", String, callback)
-  # spin() simply keeps python from exiting until this node is stopped
-  rospy.spin()
+
 if __name__ == '__main__':
-  main()
+    main()
